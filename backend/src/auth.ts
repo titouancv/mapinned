eaf50ba1e5f9f674b0016b3540d2ca610e6e3628
titouancv <titouan.carion.vignaud@gmail.com>
2025-12-29 13:55:29 +0100

    add postgres db

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,15 +1,15 @@
-import { betterAuth } from 'better-auth';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { db } from './db';
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: 'sqlite',
-  }),
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", 
+    }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
     },
-  },
 });
